2fb6e2b90d71a2a8b90f6cca77987b1786e9ed9a
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-09 18:42:22 +0900

    SET: RowSwipe Params to movies

--- src/components/Swiper/RowSwiper.tsx
+++ src/components/Swiper/RowSwiper.tsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide, Link } from 'framework7-react';
 import { useQuery } from 'react-query';
 
 import { getMovies, API_URL } from '@api';
 
-const RowSwiper = ({ categoryId, f7router }) => {
-  const { data, status, error } = useQuery('movies', getMovies(categoryId));
-  const [movies, setMovies] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await getMovies(categoryId);
-      setMovies(data);
-    })();
-  }, []);
-
-  return (
-    <Swiper navigation speed={500} slidesPerView={3} spaceBetween={20}>
-      {movies?.map((movie) => (
-        <SwiperSlide key={movie?.id}>
-          <Link href={`/movies/${movie?.id}`} className="movie-link">
-            <img src={`${API_URL}${movie?.image_path}`} alt={movie?.title} width="83.328px" height="83.328px" />
-            <div>{movie?.title}</div>
-          </Link>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-};
+const RowSwiper = ({ movies }) => (
+  <Swiper navigation speed={500} slidesPerView={3} spaceBetween={20}>
+    {movies.map((movie) => (
+      <SwiperSlide key={movie?.id}>
+        <Link href={`/movies/${movie?.id}`} className="movie-link">
+          <img src={`${API_URL}${movie?.image_path}`} alt={movie?.title} width="83.328px" height="83.328px" />
+          <div>{movie?.title}</div>
+        </Link>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+);
 
 export default RowSwiper;
