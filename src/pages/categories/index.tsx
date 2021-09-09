a0645b86b21c5334675eec6e0cdf79bec8f8b98e
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-09 18:02:17 +0900

    ADD: CategoryListPage

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Page, Toolbar } from 'framework7-react';
+import { Page, Toolbar, List, ListItem, BlockTitle } from 'framework7-react';
 import TopNavBar from '@components/TopNavBar';
 import BottomToolBarContent from '@components/BottomToolBarContent';
+import { useQuery } from 'react-query';
+import { getCategories } from '@api';
 
-const CategoryIndexPage = () => (
-  <Page className="theme-dark">
-    <TopNavBar backLink={true} />
-    <div className="py-10">This is CategoryPage</div>
-    <Toolbar tabbar labels position="bottom">
-      <BottomToolBarContent currentIdx={1} />
-    </Toolbar>
-  </Page>
-);
+const CategoryIndexPage = () => {
+  const { data: categories, status, error } = useQuery('categories', getCategories());
+
+  return (
+    <Page className="theme-dark">
+      <TopNavBar backLink={true} optionName="Category" />
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'error' && <div>{error}</div>}
+      <List id="category-list">
+        {categories &&
+          categories.map((category) => (
+            <ListItem key={category.id} title={category.title} link={`/categories/${category.id}`} />
+          ))}
+      </List>
+      <Toolbar tabbar labels position="bottom">
+        <BottomToolBarContent currentIdx={1} />
+      </Toolbar>
+    </Page>
+  );
+};
 
 export default CategoryIndexPage;
